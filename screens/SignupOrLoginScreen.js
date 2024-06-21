e7026f823006aa7fbfd75d7c961ce52f6d70e848
josivanribeiro <josivan@gmail.com>
2024-06-20 21:38:13 -0300

    Implementation of Google Signin for ios

--- screens/SignupOrLoginScreen.js
+++ screens/SignupOrLoginScreen.js
@@ -9,6 +9,7 @@ export default function SignupOrLoginScreen({ navigation }) {
   
   GoogleSignin.configure({
     androidClientId: '667042151528-aaufcddbmt736nee5gopg91je0rlg46v.apps.googleusercontent.com',
+    iosClientId:'667042151528-089p54n7asdpbqr5t2dirgog19o12nai.apps.googleusercontent.com'
   });
 
   useEffect(() => {
