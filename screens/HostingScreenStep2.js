4f5d01930feb8cd1f1c5930b3eed6de2b18b4b8f
josivanribeiro <josivan@gmail.com>
2024-09-02 22:13:02 -0300

    Implementing hosting wizard

--- screens/HostingScreenStep2.js
+++ screens/HostingScreenStep2.js
@@ -1,24 +1,68 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-const HostingScreenStep2 = () => {  
+const HostingScreenStep2 = () => {
   return (
-    <View style={styles.stepContainer}>
-      <Text style={styles.title}>Step 2</Text>
-      <Text>Welcome to step 22 of the wizard!</Text>
+    <View style={styles.container}>
+      {/* Row with the main question */}
+      <View style={styles.row}>
+        <Text style={styles.mainText}>Que tipo de lugar os hóspedes terão?</Text>
+      </View>
+
+      {/* Row with the clickable rectangle */}
+      <TouchableOpacity style={styles.clickableRectangle}>
+        <Text style={styles.subtitle}>Um lugar inteiro</Text>
+        <Text style={styles.description}>Os hóspedes têm o espaço todo só para eles.</Text>
+      </TouchableOpacity>
+      {/* Row as space */}
+      <View style={styles.spaceRow} />
+      <TouchableOpacity style={styles.clickableRectangle}>
+        <Text style={styles.subtitle}>Um quarto</Text>
+        <Text style={styles.description}>Os hóspedes têm seu próprio quarto em uma casa, além de acesso a espaços compartilhados.</Text>
+      </TouchableOpacity>
+      {/* Row as space */}
+      <View style={styles.spaceRow} />
+      <TouchableOpacity style={styles.clickableRectangle}>
+        <Text style={styles.subtitle}>Um quarto compartilhado</Text>
+        <Text style={styles.description}>Os hóspedes dormem em um quarto ou área comum que pode ser compartilhada com você ou outras pessoas.</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  stepContainer: {
-    marginBottom: 20,
-    alignItems: 'center',
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#ffffff',
   },
-  title: {
-    fontSize: 24,
-    marginBottom: 10,
-  }
+  row: {
+    marginBottom: 24,
+  },
+  mainText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  clickableRectangle: {
+    padding: 16,
+    borderRadius: 8,
+    backgroundColor: '#f0f0f0',
+    alignItems: 'flex-start',
+  },
+  subtitle: {
+    fontSize: 16,
+    fontWeight: '600',
+    marginBottom: 4,
+    color: '#000',
+  },
+  description: {
+    fontSize: 14,
+    color: '#666',
+  },
+  spaceRow: {
+    height: 20, // Adjust the height for the space row as needed
+  },
 });
 
 export default HostingScreenStep2;
--- screens/HostingScreenStep3.js
+++ screens/HostingScreenStep3.js
@@ -1,24 +1,179 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+import Colors from '../constants/colors';
 
-const HostingScreenStep3 = () => {  
+const GOOGLE_MAPS_API_KEY = 'AIzaSyBxYF0PbSFXvVuytvVgqxGwy72KQpApsLk';
+
+const HostingScreenStep3 = () => {
+  const [address, setAddress] = useState('');
+  const [street, setStreet] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [cep, setCep] = useState('');
+
+  const searchAddress = async () => {
+    if (!address) {
+      alert('Por favor, insira um endereço');
+      return;
+    }
+
+    try {
+      const response = await axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${GOOGLE_MAPS_API_KEY}`
+      );
+
+      const result = response.data.results[0];
+
+      if (result) {
+        const components = result.address_components;
+        const streetComponent = components.find(component =>
+          component.types.includes('route')
+        );
+        const cityComponent = components.find(component =>
+          component.types.includes('locality')
+        );
+        const stateComponent = components.find(component =>
+          component.types.includes('administrative_area_level_1')
+        );
+        const postalCodeComponent = components.find(component =>
+          component.types.includes('postal_code')
+        );
+
+        setStreet(streetComponent ? streetComponent.long_name : 'Desconhecido');
+        setCity(cityComponent ? cityComponent.long_name : 'Desconhecido');
+        setState(stateComponent ? stateComponent.long_name : 'Desconhecido');
+        setCep(postalCodeComponent ? postalCodeComponent.long_name : 'Desconhecido');
+      } else {
+        alert('Endereço não encontrado');
+      }
+    } catch (error) {
+      console.error(error);
+      alert('Erro ao pesquisar o endereço');
+    }
+  };
+
   return (
-    <View style={styles.stepContainer}>
-      <Text style={styles.title}>Step 3</Text>
-      <Text>Welcome to step 33 of the wizard!</Text>
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <Text style={styles.title}>Onde fica sua propriedade?</Text>
+      </View>
+
+      <View style={styles.row}>
+        <TextInput
+          style={styles.input}
+          value={address}
+          onChangeText={setAddress}
+          placeholder="Digite seu endereço"
+        />
+        <TouchableOpacity style={styles.footerButton} onPress={searchAddress}>
+          <Text style={styles.buttonText}>Pesquisar</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.row}>
+        <Text style={styles.title}>Confirme seu endereço</Text>
+      </View>
+
+      {/* Table for Street, City, and State Inputs */}
+      <View style={styles.table}>
+        <View style={styles.tableRow}>
+          <Text style={styles.label}>Rua: </Text>
+          <TextInput
+            style={styles.tableInput}
+            value={street}
+            onChangeText={setStreet}
+            placeholder="Digite a rua"
+          />
+        </View>
+        
+        <View style={styles.tableRow}>
+          <Text style={styles.label}>Cidade: </Text>
+          <TextInput
+            style={styles.tableInput}
+            value={city}
+            onChangeText={setCity}
+            placeholder="Digite a cidade"
+          />
+        </View>
+
+        <View style={styles.tableRow}>
+          <Text style={styles.label}>Estado: </Text>
+          <TextInput
+            style={styles.tableInput}
+            value={state}
+            onChangeText={setState}
+            placeholder="Digite o estado"
+          />
+        </View>
+        <View style={styles.tableRow}>
+          <Text style={styles.label}>CEP: </Text>
+          <TextInput
+            style={styles.tableInput}
+            value={cep}
+            onChangeText={setCep}
+            placeholder="Digite o CEP"
+          />
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  stepContainer: {
-    marginBottom: 20,
+  container: {
+    padding: 16,
+    backgroundColor: '#fff',
+    width: '100%',
+    flex: 1,
+  },
+  row: {
+    flexDirection: 'row',
     alignItems: 'center',
+    marginBottom: 16,
+    width: '100%',
   },
   title: {
-    fontSize: 24,
-    marginBottom: 10,
-  }
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  label: {
+    fontSize: 16,
+    marginRight: 8,
+    width: 60
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 8,
+    marginRight: 8,
+  },
+  footerButton: {
+    backgroundColor: Colors.primary500,
+    padding: 10,
+    borderRadius: 15,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  table: {
+    marginTop: 16,
+  },
+  tableRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  tableInput: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 8,
+  },
 });
 
 export default HostingScreenStep3;
